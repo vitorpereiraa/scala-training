598933fdb16b7e774253fc911d2333591eb5704c
Vitor Pereira <vitor.cbs.pereira@gmail.com>
2025-06-13 14:41:24 +0100

    added a link to the prompt

--- src/main/scala/hof/HighOrderFunction.scala
+++ src/main/scala/hof/HighOrderFunction.scala
@@ -2,16 +2,17 @@ package hof
 
 object HighOrderFunction:
   
-  def isEven(x: Int) = x % 2 == 0
+  def isEven(x: Int): Boolean = x % 2 == 0
 
-  def isOdd(x: Int) = x % 2 != 0
+  def isOdd(x: Int): Boolean = x % 2 != 0
 
-  def square(x: Int) = x * x
+  def square(x: Int): Int = x * x
 
-  def cube(x: Int) = x * x * x
+  def cube(x: Int): Int = x * x * x
 
   // The function isPrime is a predicate that checks weather a number is prime or not.
-  def isPrime(x: Int) = ???
+  // Read: https://en.wikipedia.org/wiki/Prime_number
+  def isPrime(x: Int): Boolean = ???
 
   // Function hof is supposed to receive a filter function f and mapping function m.
   def hof(f: Int => Boolean, m: Int => Int, xs: List[Int]): List[Int] = ???
