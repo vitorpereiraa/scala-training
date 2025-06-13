f5ab41bf0975ab3937e7609376147ca56aca632b
Luis Villaca <luis.villaca@ctw.bmwgroup.com>
2025-06-13 02:22:12 +0100

    chore: timestamp str - shared function

--- src/test/scala/currying/CurriedLogMsgTest.scala
+++ src/test/scala/currying/CurriedLogMsgTest.scala
@@ -7,18 +7,22 @@ import java.time.format.DateTimeFormatter
 
 class CurriedLogMsgTest extends AnyFunSuiteLike {
 
+  def getTimestampStr: String = {
+    LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+  }
+
   test("testLogMessagePartialFunctionCall") {
     val logMsgFun: (String, String) => String = ??? // partial call
 
-    val timestamp_1 = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+    val timestamp_1 = getTimestampStr
     assert(logMsgFun(timestamp_1, "Application is running.") == s"[$timestamp_1] [DEBUG] [MyApp] - Application is running.")
 
-    val timestamp_2 = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+    val timestamp_2 = getTimestampStr
     assert(logMsgFun(timestamp_2, "Application processed data.") == s"[$timestamp_2] [DEBUG] [MyApp] - Application processed data.")
   }
 
   test("testLogMessageFunctionCall") {
-    val timestamp = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+    val timestamp = getTimestampStr
 
     val logMsg: String = ???
 
